f55c2db62af4acc56431252402ce487f21380c98
Heyuan Zeng <hi@universe.observer>
2024-08-27 00:34:21 +0100

    Fix UUID init extension

--- CraftPortal/Utils/UUID+.swift
+++ CraftPortal/Utils/UUID+.swift
@@ -10,18 +10,21 @@ extension UUID {
     var flatUUIDString: String {
         uuidString.replacingOccurrences(of: "-", with: "")
     }
+}
 
+extension UUID {
     init?(flatUUIDString: String) {
         guard flatUUIDString.count == 32 else {
             return nil
         }
 
-        let formattedUUIDString: String =
-            flatUUIDString.prefix(8) + "-"
-                + flatUUIDString.dropFirst(8).prefix(4) + "-"
-                + flatUUIDString.dropFirst(12).prefix(4) + "-"
-                + flatUUIDString.dropFirst(16).prefix(4) + "-"
-                + flatUUIDString.dropFirst(20)
+        let part1 = flatUUIDString.prefix(8)
+        let part2 = flatUUIDString.dropFirst(8).prefix(4)
+        let part3 = flatUUIDString.dropFirst(12).prefix(4)
+        let part4 = flatUUIDString.dropFirst(16).prefix(4)
+        let part5 = flatUUIDString.dropFirst(20)
+
+        let formattedUUIDString = "\(part1)-\(part2)-\(part3)-\(part4)-\(part5)"
 
         self.init(uuidString: String(formattedUUIDString))
     }
